373baf748381e0d5f45fc22c003425314e6cc910
Armando <armandozarcot9@gmail.com>
2026-06-01 23:21:57 -0600

    API Render

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = window.location.origin;
 
 const contenedor = document.getElementById("contenedor-datos");
 const busqueda = document.getElementById("busqueda");
